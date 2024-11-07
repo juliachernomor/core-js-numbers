c238a1a2f7d959a18c6f2a66c6e26813171bbcd2
Iuliia Chernomorskaia <yukka.spb@gmail.com>
2024-11-08 01:42:15 +0300

    feat: finish task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -607,18 +607,8 @@ function getMaxNumber(firstNumber, secondNumber) {
  * -5, 0 => -5 | -4 | -3 | -2 | -1 | 0
  * -1, 1 => -1 | 0 | 1
  */
-function getRandomInteger(/* min, max */) {
-  // const numbers = [];
-  // for (let i = 0; i < max; i += 1) {
-  //   do {
-  //     const n = Math.floor(Math.random() * (max - min + 1)) + min;
-  //     const p = numbers.includes(n);
-  //     if (!p) {
-  //       numbers.push(n);
-  //     }
-  //   }
-  // }
-  // return numbers.join(" - ");
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -631,8 +621,8 @@ function getRandomInteger(/* min, max */) {
  * @example:
  * 3, 4 => 5
  */
-function getHypotenuse(/* a, b */) {
-  throw new Error('Not implemented');
+function getHypotenuse(a, b) {
+  return Math.hypot(a, b);
 }
 
 /**
@@ -648,8 +638,14 @@ function getHypotenuse(/* a, b */) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  let count = 0;
+  for (let i = 0; i <= Math.abs(number); i += 1) {
+    if (i % 2 !== 0) {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = {
